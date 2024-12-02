6747673d09e8366d3664f8a22178f92185891b3d
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2024-12-02 15:01:32 +0000

    fiskersvg not loading

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,15 @@
 import Header from '../components/Header';
 
-function homePage(){
+function HomePage(){
     return (
         <>
         <Header />
-        <main className="flex flex-col sm:flex-row justify-between md:gap-8"></main>
-        <section className="w=full md:w-3/4 bg-blue-400 "></section>
+        <main className="flex flex-col sm:flex-row justify-between md:gap-8">
+        <section className="w=full md:w-3/4 bg-blue-400 ">Preview Component</section>
+        <aside className="w-full md:w-1/4 bg-red-300">Configurator Component</aside>
+        </main>
         </>
-    )
+    );
 }
+
+export default HomePage;
