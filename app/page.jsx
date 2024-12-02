c9e5273ae8e3b85050aeee0debd303cdfc647fbc
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2024-12-02 13:47:31 +0000

    save commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,11 @@
+import Header from '../components/Header';
 
-function Header(){
+function homePage(){
     return (
-    <div className="text-stone-900">
-        <div className="bg-stone-100">
-            <p className="text-center font-semibold py-2">
-                All Electric &#xb7; Dyanamic &#xb7; Driving Range
-            </p>
-        </div>
-        <header className="flex justify-between px-10 py-3">
-            <image 
-            src="/fisker.svg" 
-            alt="fisker-logo"
-            height="100"
-            width="100"
-            className="w-12 sm:w-16 cursor-pointer hover:scale-110
-            transition-transform duration-100"
-            /> 
-            <span className="flex gap-2 items-center">
-          <Image src="globe.svg" width={20} height={20} alt="Globe Icon" />
-          <p className="text-sm sm:text-base">US</p>
-        </span>
-        </header>
-    </div>
+        <>
+        <Header />
+        <main className="flex flex-col sm:flex-row justify-between md:gap-8"></main>
+        <section className="w=full md:w-3/4 bg-blue-400 "></section>
+        </>
     )
 }
-
-export default Header;
---
+++ components/Header.jsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+
+function Header(){
+    return (
+    <div className="text-stone-900">
+        <div className="bg-stone-100">
+            <p className="text-center font-semibold py-2">
+                All Electric &#xb7; Dyanamic &#xb7; Driving Range
+            </p>
+        </div>
+        <header className="flex justify-between px-10 py-3">
+            <image 
+            src="/fisker.svg" 
+            alt="Fisker Logo"
+            height={100}
+            width={100}
+            className="w-12 sm:w-16 h-auto cursor-pointer hover:scale-110
+            transition-transform duration-100"
+            /> 
+            <a href="#" className="flex gap-2 items-center">
+          <Image src="globe.svg" width={20} height={20} alt="Globe Icon" />
+          <p className="text-sm sm:text-base">US</p>
+        </a>
+        </header>
+    </div>
+    )
+}
+
+export default Header;
