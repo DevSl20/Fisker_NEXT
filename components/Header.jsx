123d5f17a8a27f42ba3c4c8f85e97b7295dea2d8
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2024-12-03 07:52:11 +0000

    save configurator changes

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,29 +1,36 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-function Header(){
-    return (
+function Header() {
+  return (
     <div className="text-stone-900">
-        <div className="bg-stone-100">
-            <p className="text-center font-semibold py-2">
-                All Electric &#xb7; Dyanamic &#xb7; Driving Range
-            </p>
-        </div>
-        <header className="flex justify-between px-10 py-3">
-            <image 
-            src="/fisker.svg" 
-            alt="Fisker Logo"
-            height={100}
-            width={100}
-            className="w-12 sm:w-16 h-auto cursor-pointer hover:scale-110
+      <div className="bg-stone-100">
+        <p className="text-center font-semibold py-2">
+          All Electric &#xb7; Dyanamic &#xb7; Driving Range
+        </p>
+      </div>
+
+      {/* Header */}
+      <header className="flex justify-between px-10 py-3">
+        <Image
+          src="fisker.svg"
+          alt="Fisker Logo"
+          height={100}
+          width={100}
+          className="w-12 sm:w-16 h-auto cursor-pointer hover:scale-110
             transition-transform duration-100"
-            /> 
-            <a href="#" className="flex gap-2 items-center">
+        />
+        <h1 className="text-2xl md:text-6xl font-semibold text-blue-950">
+          Fisker Ocean
+        </h1>
+
+        {/*Region*/}
+        <a href="#" className="flex gap-2 items-center">
           <Image src="globe.svg" width={20} height={20} alt="Globe Icon" />
           <p className="text-sm sm:text-base">US</p>
         </a>
-        </header>
+      </header>
     </div>
-    )
+  );
 }
 
 export default Header;
