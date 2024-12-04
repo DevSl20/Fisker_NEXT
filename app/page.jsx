fbcb8487062b8a9897fddf82ea0cedb9eb668abb
𝐃𝐄𝐕𝐀𝐍𝐆 𝐒𝐀𝐋𝐕𝐈 <devangsalvi21@gmail.com>
2024-12-04 07:09:03 +0000

    padding config

--- app/page.jsx
+++ app/page.jsx
@@ -8,7 +8,7 @@ function HomePage(){
         <Header />
         <main className="flex flex-col sm:flex-row justify-between md:gap-8 px-2 md:px-8 py-3 md:py-6">
         <section className="w=full md:w-3/4 "><Preview/></section>
-        <aside className="w-full md:w-1/4 bg-red-300"><Configurator/></aside>
+        <aside className="w-full md:w-1/4 bg-red-300 px-2"><Configurator/></aside>
         </main>
         </>
     );
